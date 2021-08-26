5abc5747fe9420a5a64313cbaf319afe48938bc5
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-08-26 17:16:35 -0400

    clean up

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -41,7 +41,7 @@ class StreamList extends Component {
     if (this.props.isSignedIn) {
       return (
         <div style={{ textAlign: "right" }}>
-          <Link to={"/streams/new"} className="ui button primary">
+          <Link to="/streams/new" className="ui button primary">
             Create Stream
           </Link>
         </div>
