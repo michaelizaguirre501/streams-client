894d366c9a415b889a0120d8540e6b7bf0672bce
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-09-07 23:43:02 -0400

    map state to props to display the title of the steream in the delete modal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Modal from "../Modal";
 import history from "../../history";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
 class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+    console.log(this.props);
+  }
   renderActions() {
     return (
       <>
@@ -17,7 +23,7 @@ class StreamDelete extends React.Component {
         StreamDelete
         <Modal
           header="Delete Stream"
-          content="Are you sure you want to delete this stream?"
+          content={`Are you sure you want to delete the stream ${this.props.stream.title}?`}
           actions={this.renderActions()}
           onDismiss={() => history.push("/")}
         />
@@ -25,5 +31,7 @@ class StreamDelete extends React.Component {
     );
   }
 }
-
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+export default connect(mapStateToProps, { fetchStream })(StreamDelete);
