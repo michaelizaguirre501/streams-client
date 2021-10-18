0b7ed9b231687b4cd436864b0e8dfffaa2fec23b
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-10-18 12:53:40 -0400

    Header started

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>NAVBAR</div>;
+  return (
+    <div className="ui secondary pointing menu">
+      <Link className="item" to="/">
+        Stream Me Up
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Streams
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
