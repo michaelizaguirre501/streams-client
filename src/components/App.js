6d46a97836f76e460f0efef4b82e6fbfd99b5d9b
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-10-18 12:02:52 -0400

    scaffolding

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
