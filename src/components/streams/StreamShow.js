b8b177025fa5738445e527a07cff8d93abe5a4bc
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-10-31 11:28:13 -0400

    added switch to app.js to only display one route at a time

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -5,6 +5,7 @@ import { fetchStream } from "../../actions";
 class StreamShow extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+    console.log(this.props);
   }
   render() {
     return <div>StreamShow</div>;
